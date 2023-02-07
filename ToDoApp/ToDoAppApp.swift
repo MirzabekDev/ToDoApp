8ca0757978e94341a5fe117445ebc54667fc3c01
Mirzabek <mirzabekmirzayev746@gmail.com>
2023-02-07 22:20:09 +0500

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Mirzabek on 07/02/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
